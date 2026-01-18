5c4e99031906501e596e56bee52ba98b29d46906
mohit012x <sharmams021@gmail.com>
2026-01-18 20:00:06 +0530

    Updated

---
+++ eco-track-exp1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Title from "./pages/Title";
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <>
+      <Title title = "Carbon footprint by activities"/> 
+      <Dashboard/>
+    </>
+  )
+}
+
+export default App
